82788693ef2906cf0946f387c7cebee3b59c1446
Von Claudio <vonclaudio.college@gmail>
2026-05-20 10:33:09 +0800

    feat: initializing quiz app project structure

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const questions = [
+    {
+        question: "What does the CPU stand for?",
+        answers: [
+            "Central Processing Unit", 
+            "Computer Personal Unit", 
+            "Central Performance Unit", 
+            "Control Processing Unit"
+        ],
+        correct: 0
+    },
+
+    {
+        question: "What does RAM stand for?",
+        answers: [
+            "Read Access Memory",
+            "Run Access Memory",
+            "Random Access Memory",
+            "Random Active Memory"
+        ],
+        correct: 2
+    },
+
+    {
+        question: "What is the purpose of a resistor in an electronic circuit?",
+        answers: [
+            "To store electrical energy",
+            "To limit the flow of electrical current",
+            "To increase the voltage in a circuit",
+            "To convert AC to DC"
+        ],
+        correct: 1
+    },
+
+    {
+        question: "What does GPU stand for?",
+        answers: [
+            "General Processing Unit",
+            "Graphics Performance Unit",
+            "Graphics Processing Unit",
+            "General Performance Unit"
+        ],
+        correct: 2
+    },
+
+    {
+        question: "How many main buses are there in a computer?",
+        answers: [
+            "1",
+            "2",    
+            "3",
+            "4"
+        ],
+        correct: 2
+    }
+]
